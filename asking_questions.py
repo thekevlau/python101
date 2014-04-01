52f3d0550c41752971bb38e78c43ddf4bed74034
Kevin Lau <thekevlau@gmail.com>
2014-04-01 19:04:23 -0400

    update

--- asking_questions.py
+++ asking_questions.py
@@ -1,10 +1,5 @@
-print "how old are you?",
-age = raw_input()
-print "how tall are you?"
-height = raw_input()
-print "how much do you weight?",
-weight = raw_input()
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you ")
+weight = raw_input("How much do you weight ")
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
-
-
